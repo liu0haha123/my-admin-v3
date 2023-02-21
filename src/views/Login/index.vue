78d7fadc3b9af295123ac765afdc7d0a4d683211
lw <1109160884@qq.com>
2023-02-22 00:37:09 +0800

    初始化登录页面

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,10 +1,81 @@
 <template>
-  <div>
-    LOGIN
-    <t-button>测试</t-button>
+  <div class="login-container">
+    <div class="content">
+      <t-card>
+        <h1>My Admin</h1>
+        <t-form
+          ref="form"
+          :data="loginForm"
+          :rules="rules"
+          class="login-form"
+          :colon="true"
+          :label-width="0"
+        >
+          <t-form-item name="username">
+            <t-input
+              v-model="loginForm.username"
+              clearable
+              placeholder="请输入用户名"
+            >
+              <template #prefix-icon>
+                <icon name="desktop" />
+              </template>
+            </t-input>
+          </t-form-item>
+          <t-form-item name="password">
+            <t-input
+              v-model="loginForm.password"
+              type="password"
+              clearable
+              placeholder="请输入密码"
+            >
+              <template #prefix-icon>
+                <icon name="lock-on" />
+              </template>
+            </t-input>
+          </t-form-item>
+          <t-form-item style="padding-top: 8px">
+            <t-button theme="primary" type="submit" block>登录</t-button>
+          </t-form-item>
+        </t-form>
+      </t-card>
+    </div>
   </div>
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import { Icon } from "tdesign-vue-next";
+import { reactive } from "vue";
+const rules = {
+  username: [{ required: true, message: "请填写用户名" }],
+  password: [{ required: true, message: "请填写密码" }],
+};
+type LoginForm = {
+  username: string;
+  password: string;
+};
+const loginForm = reactive<LoginForm>({
+  username: "",
+  password: "",
+});
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="less" scoped>
+.login-container {
+  width: 100vw;
+  height: 100vh;
+  background-color: #f5f5f5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .content {
+    width: 400px;
+    h1 {
+      text-align: center;
+    }
+    .login-form {
+      margin: 20px 0;
+    }
+  }
+}
+</style>
